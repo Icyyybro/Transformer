98f743aefdee7da5e8f932fb9140ec3aba44654f
YiChuanWang <1425837933@qq.com>
2024-07-20 23:16:34 +0800

    LayerNorm and FeedForward

---
+++ LayerNorm.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.autograd import Variable
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        # features为特征维度，Transformer中指d_model
+        # eps为了防止分母为0
+        super(LayerNorm, self).__init__()
+
+        # 设置两个可训练的变量a2和b2，其实就是归一化后需要缩放和偏移的参数
+        # 这两个参数是可训练的
+        self.a2 = nn.Parameter(torch.ones(features))
+        self.b2 = nn.Parameter(torch.zeros(features))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        # 这里的乘法是两个相同形状的tensor的对应位置的数做乘法，和matmul矩阵乘法有区别
+        return self.a2 * (x - mean) / (std + self.eps) + self.b2
